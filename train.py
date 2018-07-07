933304abd455b841cf79310e0ddc1c4dc9fdef2f
zhaoguangyao <vipzgy@gmail.com>
2018-07-07 16:17:01 +0800

    update

--- train.py
+++ train.py
@@ -7,11 +7,11 @@ import argparse
 from TorchNN import *
 from driver.Config import Configurable
 from driver.IO import read_pkl
-from driver.Vocab import PAD
+from driver.Vocab import PAD, VocabSrc, VocabTgt
 from driver.Train import train
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-os.environ["OMP_NUM_THREADS"] = "1"
+# os.environ["OMP_NUM_THREADS"] = "1"
 
 
 if __name__ == '__main__':
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     parse = argparse.ArgumentParser('Attention Target Classifier')
     parse.add_argument('--config_file', type=str, default='default.ini')
     parse.add_argument('--thread', type=int, default=1)
-    parse.add_argument('--use_cuda', action='store_true', default=True)
+    parse.add_argument('--use_cuda', action='store_true', default=False)
     args, extra_args = parse.parse_known_args()
 
     config = Configurable(args.config_file, extra_args)
@@ -45,22 +45,28 @@ if __name__ == '__main__':
     dev_data = read_pkl(config.dev_pkl)
     test_data = read_pkl(config.test_pkl)
 
-    feature_vec = read_pkl(config.load_feature_voc)
-    label_vec = read_pkl(config.load_label_voc)
+    feature_list = read_pkl(config.load_feature_voc)
+    label_list = read_pkl(config.load_label_voc)
+    feature_voc = VocabSrc(feature_list)
+    label_voc = VocabTgt(label_list)
+
     embedding = None
     if os.path.isfile(config.embedding_pkl):
         embedding = read_pkl(config.embedding_pkl)
 
     # model
     if config.which_model == 'Vanilla':
-        model = Vanilla(config, feature_vec.size, embedding[1] if embedding else config.embed_dim,
-                        PAD, label_vec.size, embedding[0])
+        model = Vanilla(config, feature_voc.size,
+                        embedding[1] if embedding else config.embed_dim,
+                        PAD, label_voc.size, embedding[0])
     elif config.which_model == 'Contextualized':
-        model = Contextualized(config, feature_vec.size, embedding[1] if embedding else config.embed_dim,
-                               PAD, label_vec.size, embedding[0])
+        model = Contextualized(config, feature_voc.size,
+                               embedding[1] if embedding else config.embed_dim,
+                               PAD, label_voc.size, embedding[0])
     elif config.which_model == 'ContextualizedGates':
-        model = ContextualizedGates(config, feature_vec.size, embedding[1] if embedding else config.embed_dim,
-                                    PAD, label_vec.size, embedding[0])
+        model = ContextualizedGates(config, feature_voc.size,
+                                    embedding[1] if embedding else config.embed_dim,
+                                    PAD, label_voc.size, embedding[0])
     else:
         print('please choose right model')
         exit()
@@ -69,5 +75,5 @@ if __name__ == '__main__':
         model = model.cuda()
 
     # train
-    train(model, train_data, dev_data, test_data, feature_vec, label_vec, config)
+    train(model, train_data, dev_data, test_data, feature_voc, label_voc, config)
 
